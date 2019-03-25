c9d2414c0276cab84177d221a7ae960e2e6b4cb4
Mohammad Kaouri <moe@fusionsecond.com>
2019-03-25 15:23:46 +0200

    init command

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    const headers= {
+      'Access-Control-Allow-Origin': 'no-cors'
+    }
+
+
+    fetch('http://127.0.0.1:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
